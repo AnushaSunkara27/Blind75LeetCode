2e974c9967e101888363d39de7ac63fe84015029
AnushaSunkara27 <anusha.vjcf1@gmail.com>
2024-01-03 22:43:16 -0600

    Linked list hasCycle

--- LinkedList/ReverseLinkedList.java
+++ LinkedList/ReverseLinkedList.java
@@ -1,5 +1,27 @@
 package LinkedList;
 
 public class ReverseLinkedList {
+
+    public class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = head;  
+        ListNode prev = null;  
+
+       while(temp != null){  
+
+           ListNode front = temp.next;  
+           temp.next = prev;  
+           prev = temp;  
+           temp = front; 
+        }
+   
+        return prev;  
+    }
 }
