21a69511ff431d6fc56f74cf9e9402f14fa10e50
AnushaSunkara27 <anusha.vjcf1@gmail.com>
2024-01-09 21:25:44 -0600

    Is same tree implementation

--- Trees/IdenticalTrees.java
+++ Trees/IdenticalTrees.java
@@ -1,5 +1,31 @@
 package Trees;
 
 public class IdenticalTrees {
-    
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public boolean isIdentical(Node root, Node subRoot) {
+        if(root == null && subRoot == null) {
+            return true;
+        }
+
+        if(root == null || subRoot == null) {
+            return false;
+        }
+
+        if(root.data == subRoot.data) {
+            return isIdentical(root.left, subRoot.left) && isIdentical(root.right, subRoot.right);
+        }
+
+        return false;
+    }
 }
